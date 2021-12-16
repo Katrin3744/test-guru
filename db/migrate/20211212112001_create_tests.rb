99c34a77c30689a96631df3828f76a6a8d7e987f
Katrin3744 <k.kukh3477@gmail.com>
2021-12-16 18:20:12 +0300

    tz2 changes in migrations v2

--- db/migrate/20211212112001_create_tests.rb
+++ db/migrate/20211212112001_create_tests.rb
@@ -2,7 +2,7 @@ class CreateTests < ActiveRecord::Migration[6.1]
   def change
     create_table :tests do |t|
       t.string :title, null: false
-      t.integer :level, :default => 0
+      t.integer :level, default: 0
       t.references :category, null: false, foreign_key: true
       t.references :author, null: false, index: true, foreign_key: { to_table: :users }
       t.timestamps
