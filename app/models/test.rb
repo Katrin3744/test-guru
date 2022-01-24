b3e04a3892ed008fe93fa3e74af58ea4d1ca1669
Katrin3744 <k.kukh3477@gmail.com>
2022-01-24 13:29:12 +0300

    tz4 changed query and dependencies

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,7 +2,7 @@ class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, foreign_key: "author_id", class_name: "User"
 
-  has_many :questions, dependent: :delete_all
+  has_many :questions, dependent: :destroy
   has_many :tests_users, dependent: :delete_all
   has_many :users, through: :tests_users, dependent: :destroy
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ApplicationRecord
   has_many :tests_users, dependent: :delete_all
   has_many :performed_tests, through: :tests_users, source: :test, dependent: :destroy
-  has_many :written_tests, foreign_key: "author_id", class_name: "Test", dependent: :delete_all
+  has_many :written_tests, foreign_key: "author_id", class_name: "Test", dependent: :destroy
 
   def tests_with_level(level)
-    Test.joins(:tests_users)
-        .where(tests: { level: level }, tests_users: { user_id: self.id })
+    performed_tests.where(tests: { level: level })
   end
 end
