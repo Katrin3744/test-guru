5de229bef3698638221bab729bae9e23676c6bd4
Katrin3744 <k.kukh3477@gmail.com>
2022-02-08 21:11:12 +0300

    tz5 change validation in answers

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,13 +2,13 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :body, presence: true
-  validate :validate_answers_number
+  validate :validate_answers_number, on: :create
 
   scope :correct, -> { where(correct: true) }
 
   private
 
   def validate_answers_number
-    self.errors.add(:base, "too much answers") if self.question.answers.count > 3
+    errors.add(:base, "too much answers") if question.answers.count >= 4
   end
 end
