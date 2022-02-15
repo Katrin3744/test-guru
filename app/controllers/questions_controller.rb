ad3610f5db8e108443e40b3d0130098ef931c8d1
Katrin3744 <k.kukh3477@gmail.com>
2022-02-15 14:13:01 +0300

    tz6 delete

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,16 +1,35 @@
 class QuestionsController < ApplicationController
   before_action :find_test
 
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_record_not_found
+
   def index
   end
 
+  def new
+  end
+
+  def create
+    @question = Question.create!({body: params[:question][:body], test_id: params[:test_id].to_i})
+    render plain: 'Question was created!'
+  end
+
   def show
-    render inline: '<%=@test.questions.find(params[:id]).body%>'
+  end
+
+  def destroy
+    @question =Question.find(params[:id])
+    @question.destroy
+    render plain: 'Question was delete!'
   end
 
   private
 
   def find_test
     @test =Test.find(params[:test_id])
+  end
+
+  def rescue_with_record_not_found
+    render plain: 'Question was not found'
   end
 end
