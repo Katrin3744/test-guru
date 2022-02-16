7d79e9406ec4f5b33f8c605f45ff27d4054627cf
Katrin3744 <k.kukh3477@gmail.com>
2022-02-16 16:53:15 +0300

    tz6 changed

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,8 @@
 class QuestionsController < ApplicationController
   before_action :find_test
+  before_action :find_question
+  skip_before_action :find_test, only: [:show, :destroy]
+  skip_before_action :find_question, only: [:create, :index, :new]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_record_not_found
 
@@ -10,15 +13,19 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    @question = Question.create!({body: params[:question][:body], test_id: params[:test_id].to_i})
-    render plain: 'Question was created!'
+    @question_new = @test.questions.build({ body: params[:question][:body], test: @test })
+    if @question_new.save
+      redirect_to @question_new
+    else
+      render :new
+    end
   end
 
   def show
+    @question_by_id = @question.body
   end
 
   def destroy
-    @question =Question.find(params[:id])
     @question.destroy
     render plain: 'Question was delete!'
   end
@@ -26,7 +33,11 @@ class QuestionsController < ApplicationController
   private
 
   def find_test
-    @test =Test.find(params[:test_id])
+    @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
   end
 
   def rescue_with_record_not_found
--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,5 +1,2 @@
 class TestsController < ApplicationController
-  def index
-
-  end
 end
