aa491e2a4dd9904632a789d3188376afacb0538c
Katrin3744 <k.kukh3477@gmail.com>
2022-02-23 15:37:32 +0300

    tz8 change edit for answers, delete instance variables in test_passage controller for show, change correct answer method

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -2,7 +2,6 @@ class TestPassagesController < ApplicationController
   before_action :find_test_passage, only: [:show, :result, :update]
 
   def show
-    calculate_number_of_questions
   end
 
   def result
@@ -15,7 +14,6 @@ class TestPassagesController < ApplicationController
     if @test_passage.completed?
       redirect_to result_test_passage_path(@test_passage)
     else
-      calculate_number_of_questions
       render :show
     end
   end
@@ -24,10 +22,5 @@ class TestPassagesController < ApplicationController
 
   def find_test_passage
     @test_passage = TestPassage.find(params[:id])
-  end
-
-  def calculate_number_of_questions
-    @number_of_questions = @test_passage.number_of_questions
-    @question_number = @test_passage.current_question_number
   end
 end
--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -35,7 +35,7 @@ class TestPassage < ApplicationRecord
   end
 
   def correct_answer?(answer_ids)
-    correct_answers.ids.sort == answer_ids.map(&:to_i).sort if !answer_ids.nil?
+    correct_answers.ids.sort == answer_ids.to_a.map(&:to_i).sort
   end
 
   def correct_answers
