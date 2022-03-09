93424f99db1abb39304a068fa777defeebe80f22
Katrin3744 <k.kukh3477@gmail.com>
2022-03-09 18:56:55 +0300

    tz11 created controller for gists, change GistQuestionService, change flash messages helper and translation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,7 @@ module ApplicationHelper
     link_to title, "https://github.com/#{author}/#{repo}", target: '_blank'
   end
 
-  def flash_message
+  def flash_messages
     flash.map { |key, msg| (content_tag :p, sanitize(msg, { attributes: %w(target href) }),
                                         :id => key, :class => 'flash ' + key) }.join.html_safe
   end
