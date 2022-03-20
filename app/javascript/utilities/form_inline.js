e1400c5fce9ebc1cf19b8724a6453105356a5a6a
Katrin3744 <k.kukh3477@gmail.com>
2022-03-20 20:51:16 +0300

    tz13 fix error with js on edit page for test

--- app/javascript/utilities/form_inline.js
+++ app/javascript/utilities/form_inline.js
@@ -1,16 +1,15 @@
 document.addEventListener('turbolinks:load', function () {
-    console.log("++++++++")
     const controls = document.querySelectorAll('.form-inline-link')
     if (controls.length) {
         for (let i = 0; i < controls.length; i++) {
             controls[i].addEventListener('click', formInlineLinkHandler)
         }
-    }
-    const errors = document.querySelector('.resource-errors')
+        const errors = document.querySelector('.resource-errors')
 
-    if (errors) {
-        const resourceId = errors.dataset.resourceId
-        formInlineHandler(resourceId)
+        if (errors) {
+            const resourceId = errors.dataset.resourceId
+            formInlineHandler(resourceId)
+        }
     }
 })
 
@@ -25,7 +24,6 @@ function formInlineHandler(testId) {
     const link = document.querySelector('.form-inline-link[data-test-id="' + testId + '"]')
     const testTitle = document.querySelector('.test-title[data-test-id="' + testId + '"]')
     const formInline = document.querySelector('.form-inline[data-test-id="' + testId + '"]')
-    console.log(formInline)
 
 
     if (formInline.classList.contains('hide')) {
