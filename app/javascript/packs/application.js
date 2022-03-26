1d81e501dba4e0b58e21b2ef4bd02b1c772c0924
Katrin3744 <k.kukh3477@gmail.com>
2022-03-26 21:00:32 +0300

    fix after cherry-pick

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,7 +11,6 @@ import "utilities/sorting"
 import "utilities/password_match_check"
 import "utilities/form_inline"
 import "utilities/progress_bar"
-import "utilities/adding_search_condition"
 import "utilities/test_timer_tracking"
 
 Rails.start()
