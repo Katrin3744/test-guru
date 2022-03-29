7c6e955388d15905b972a65918b4a1ed91d361c8
Katrin3744 <k.kukh3477@gmail.com>
2022-03-29 22:02:18 +0300

    tz14 code style

--- app/specification/badges/finish_all_tests_category.rb
+++ app/specification/badges/finish_all_tests_category.rb
@@ -2,10 +2,9 @@ class Badges::FinishAllTestsCategory < AbstractRuleSpecification
   def satisfies?
     tests_with_category = Test.where(category_id: @value)
     success_user_tests_with_category = tests_with_category.joins(:test_passages)
-                                            .where("user_id = :user_id and result >= :result",
-                                                   user_id: @test_passage.user_id,
-                                                   result: TestPassage::SUCCESS_RATIO).distinct
+                                                          .where("user_id = :user_id and result >= :result",
+                                                                 user_id: @test_passage.user_id,
+                                                                 result: TestPassage::SUCCESS_RATIO).distinct
     tests_with_category.count == success_user_tests_with_category.count
   end
 end
-
--- app/specification/badges/finish_all_tests_with_level.rb
+++ app/specification/badges/finish_all_tests_with_level.rb
@@ -2,9 +2,9 @@ class Badges::FinishAllTestsWithLevel < AbstractRuleSpecification
   def satisfies?
     tests_with_level = Test.where(level: @value)
     success_user_tests_with_level = tests_with_level.joins(:test_passages)
-                                         .where("user_id = :user_id and result >= :result",
-                                                user_id: @test_passage.user_id,
-                                                result: TestPassage::SUCCESS_RATIO).distinct
+                                                    .where("user_id = :user_id and result >= :result",
+                                                           user_id: @test_passage.user_id,
+                                                           result: TestPassage::SUCCESS_RATIO).distinct
     tests_with_level.count == success_user_tests_with_level.count
   end
 end
